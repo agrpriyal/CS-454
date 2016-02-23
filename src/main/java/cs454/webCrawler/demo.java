4a9ba6ec46d25b64c86acd30977289a4fc5a9826
ElTangoBandito <Volfurious@gmail.com>
2016-02-22 18:09:08 -0800

    WordCounter Completed
    WordCounter returns a JSON object that has all the info for one html
    page.

--- src/main/java/cs454/webCrawler/demo.java
+++ src/main/java/cs454/webCrawler/demo.java
@@ -4,13 +4,23 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.tika.exception.TikaException;
+import org.json.simple.JSONArray;
 import org.xml.sax.SAXException;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class demo {
+	static JSONArray jsonArray = new JSONArray();
+	
 	public static void main( String[] args ) throws IOException, SAXException, TikaException
     {
 		File file = new File("C:/Users/Volfurious/Desktop/test.html");//file path
 		WordCounter word = new WordCounter(file);
-		System.out.println(word.getWords());
+		//System.out.println(word.getJson());
+		
+		Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
+		String pretJson = prettyGson.toJson(word.getJson());
+		System.out.println(pretJson);
     }
 }
